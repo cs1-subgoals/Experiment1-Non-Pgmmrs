4be94e257e44e1c6ea5594fb8517c52e9756bf2a
waddillp <wadplattuva@gmail.com>
2025-04-24 15:18:33 -0400

    Latest build of target runestone.

---
+++ _static/lunr-pretext-search-index.js
@@ -0,0 +1,103 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "consent-form",
+  "level": "1",
+  "url": "consent-form.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Consent Form",
+  "body": " Consent Form  Please read the following consent form.       Q1: Do you consent to participating in the research as described to you in the consent document?     Yes          No    You must consent to participate      "
+},
+{
+  "id": "Consent-Form",
+  "level": "2",
+  "url": "consent-form.html#Consent-Form",
+  "type": "Exercise",
+  "number": "1.1.1",
+  "title": "",
+  "body": " Q1: Do you consent to participating in the research as described to you in the consent document?     Yes          No    You must consent to participate    "
+},
+{
+  "id": "survey",
+  "level": "1",
+  "url": "survey.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Survey",
+  "body": " Survey  Please answer the following survey questions.    Q1: Please indicate which, if any, prior computer programming experiences that you've had during grades K–5 (age 5 to 11) — check ALL that apply. (The checkme results can be ignored!)    Less than a day (museum type)    More than a day but less than 2 months outside of school (camps)    More than a day but less than 2 months inside of school (programming unit)    More than a day but less than 2 months outside of school (learning on your own)    More than 2 months outside of school (after school program)    More than 2 months inside of school (computing course)    More than 2 months outside of school (learning on your own)     Q2: Please indicate which, if any, prior computer programming experiences that you’ve had during grades 6–8 (age 12 to 14) — check ALL that apply.    Less than a day (museum type)    More than a day but less than 2 months outside of school (camps)    More than a day but less than 2 months inside of school (programming unit)    More than a day but less than 2 months outside of school (learning on your own)    More than 2 months outside of school (after school program)    More than 2 months inside of school (computing course)    More than 2 months outside of school (learning on your own)     Q3: Please indicate which, if any, prior computer programming experiences that you’ve had during grades 9–12 (age 14 to 18) — check ALL that apply.    Less than a day (museum type)    More than a day but less than 2 months outside of school (camps)    More than a day but less than 2 months inside of school (programming unit)    More than a day but less than 2 months outside of school (learning on your own)    More than 2 months outside of school (after school program)    More than 2 months inside of school (computing course)    More than 2 months outside of school (learning on your own)     Q4: Please indicate which, if any, courses you have taken here at UB that involved programming — check ALL that apply.    CSE 111    CSE 113    CSE 115    CSE 116    CSE 2xx course (any)    None of the above     Q5: Based on your previous CS experience, how much do you agree or disagree with the following statement? [ONLY USED FOR STUDENTS IN CSE 115 & 116]    I am better at computer science than most of the other students in my course.     1 - Completely Disagree    2 - Mostly Disagree    3 - Slightly Disagree    4 - Slightly Agree    5 - Mostly Agree    6 - Completely Agree    Not a student of CSE 115 or 116.     Q6: Based on your previous CS experience, how much do you agree or disagree with the following statement? [ONLY USED FOR STUDENTS IN CSE 115 & 116]    I am very good at computer science.     1 - Completely Disagree    2 - Mostly Disagree    3 - Slightly Disagree    4 - Slightly Agree    5 - Mostly Agree    6 - Completely Agree    Not a student of CSE 115 or 116.     Q7: Based on your previous CS experience, how much do you agree or disagree with the following statement? [ONLY USED FOR STUDENTS IN CSE 115 & 116]    I can figure out how to solve the most difficult problems in my computer science class if I try.     1 - Completely Disagree    2 - Mostly Disagree    3 - Slightly Disagree    4 - Slightly Agree    5 - Mostly Agree    6 - Completely Agree    Not a student of CSE 115 or 116.     "
+},
+{
+  "id": "survey-Q1",
+  "level": "2",
+  "url": "survey.html#survey-Q1",
+  "type": "Exercise",
+  "number": "1.2.1",
+  "title": "",
+  "body": " Q1: Please indicate which, if any, prior computer programming experiences that you've had during grades K–5 (age 5 to 11) — check ALL that apply. (The checkme results can be ignored!)    Less than a day (museum type)    More than a day but less than 2 months outside of school (camps)    More than a day but less than 2 months inside of school (programming unit)    More than a day but less than 2 months outside of school (learning on your own)    More than 2 months outside of school (after school program)    More than 2 months inside of school (computing course)    More than 2 months outside of school (learning on your own)   "
+},
+{
+  "id": "survey-Q2",
+  "level": "2",
+  "url": "survey.html#survey-Q2",
+  "type": "Exercise",
+  "number": "1.2.2",
+  "title": "",
+  "body": " Q2: Please indicate which, if any, prior computer programming experiences that you’ve had during grades 6–8 (age 12 to 14) — check ALL that apply.    Less than a day (museum type)    More than a day but less than 2 months outside of school (camps)    More than a day but less than 2 months inside of school (programming unit)    More than a day but less than 2 months outside of school (learning on your own)    More than 2 months outside of school (after school program)    More than 2 months inside of school (computing course)    More than 2 months outside of school (learning on your own)   "
+},
+{
+  "id": "survey-Q3",
+  "level": "2",
+  "url": "survey.html#survey-Q3",
+  "type": "Exercise",
+  "number": "1.2.3",
+  "title": "",
+  "body": " Q3: Please indicate which, if any, prior computer programming experiences that you’ve had during grades 9–12 (age 14 to 18) — check ALL that apply.    Less than a day (museum type)    More than a day but less than 2 months outside of school (camps)    More than a day but less than 2 months inside of school (programming unit)    More than a day but less than 2 months outside of school (learning on your own)    More than 2 months outside of school (after school program)    More than 2 months inside of school (computing course)    More than 2 months outside of school (learning on your own)   "
+},
+{
+  "id": "survey-Q4",
+  "level": "2",
+  "url": "survey.html#survey-Q4",
+  "type": "Exercise",
+  "number": "1.2.4",
+  "title": "",
+  "body": " Q4: Please indicate which, if any, courses you have taken here at UB that involved programming — check ALL that apply.    CSE 111    CSE 113    CSE 115    CSE 116    CSE 2xx course (any)    None of the above   "
+},
+{
+  "id": "survey-Q5",
+  "level": "2",
+  "url": "survey.html#survey-Q5",
+  "type": "Exercise",
+  "number": "1.2.5",
+  "title": "",
+  "body": " Q5: Based on your previous CS experience, how much do you agree or disagree with the following statement? [ONLY USED FOR STUDENTS IN CSE 115 & 116]    I am better at computer science than most of the other students in my course.     1 - Completely Disagree    2 - Mostly Disagree    3 - Slightly Disagree    4 - Slightly Agree    5 - Mostly Agree    6 - Completely Agree    Not a student of CSE 115 or 116.   "
+},
+{
+  "id": "survey-Q6",
+  "level": "2",
+  "url": "survey.html#survey-Q6",
+  "type": "Exercise",
+  "number": "1.2.6",
+  "title": "",
+  "body": " Q6: Based on your previous CS experience, how much do you agree or disagree with the following statement? [ONLY USED FOR STUDENTS IN CSE 115 & 116]    I am very good at computer science.     1 - Completely Disagree    2 - Mostly Disagree    3 - Slightly Disagree    4 - Slightly Agree    5 - Mostly Agree    6 - Completely Agree    Not a student of CSE 115 or 116.   "
+},
+{
+  "id": "survey-Q7",
+  "level": "2",
+  "url": "survey.html#survey-Q7",
+  "type": "Exercise",
+  "number": "1.2.7",
+  "title": "",
+  "body": " Q7: Based on your previous CS experience, how much do you agree or disagree with the following statement? [ONLY USED FOR STUDENTS IN CSE 115 & 116]    I can figure out how to solve the most difficult problems in my computer science class if I try.     1 - Completely Disagree    2 - Mostly Disagree    3 - Slightly Disagree    4 - Slightly Agree    5 - Mostly Agree    6 - Completely Agree    Not a student of CSE 115 or 116.   "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
