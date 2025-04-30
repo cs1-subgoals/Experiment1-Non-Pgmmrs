867d35c6fd483528f1a6675325a4f03e3d112550
waddillp <wadplattuva@gmail.com>
2025-04-30 01:30:57 -0400

    Latest build of target runestone.

--- _static/lunr-pretext-search-index.js
+++ _static/lunr-pretext-search-index.js
@@ -7,14 +7,23 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.1",
   "title": "Consent Form",
-  "body": " Consent Form  Please read the following consent form.       Q1: Do you consent to participating in the research as described to you in the consent document?     Yes          No    You must consent to participate      "
+  "body": " Consent Form  Please read the following consent form.       Q1: Have you been given a copy of the consent document for this research?     Yes          No    You must consent to participate      Q1: Do you consent to participating in the research as described to you in the consent document?     Yes          No    You must consent to participate      "
 },
 {
-  "id": "Consent-Form",
+  "id": "Consent-FormQ1",
   "level": "2",
-  "url": "consent-form.html#Consent-Form",
+  "url": "consent-form.html#Consent-FormQ1",
   "type": "Exercise",
   "number": "1.1.1",
+  "title": "",
+  "body": " Q1: Have you been given a copy of the consent document for this research?     Yes          No    You must consent to participate    "
+},
+{
+  "id": "Consent-FormQ2",
+  "level": "2",
+  "url": "consent-form.html#Consent-FormQ2",
+  "type": "Exercise",
+  "number": "1.1.2",
   "title": "",
   "body": " Q1: Do you consent to participating in the research as described to you in the consent document?     Yes          No    You must consent to participate    "
 },
@@ -89,6 +98,60 @@ var ptx_lunr_docs = [
   "number": "1.2.7",
   "title": "",
   "body": " Q7: Based on your previous CS experience, how much do you agree or disagree with the following statement? [ONLY USED FOR STUDENTS IN CSE 115 & 116]    I can figure out how to solve the most difficult problems in my computer science class if I try.     1 - Completely Disagree    2 - Mostly Disagree    3 - Slightly Disagree    4 - Slightly Agree    5 - Mostly Agree    6 - Completely Agree    Not a student of CSE 115 or 116.   "
+},
+{
+  "id": "write_conditional",
+  "level": "1",
+  "url": "write_conditional.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Write Conditional",
+  "body": " Write Conditional   Subgoals for writing conditionals:     Define how many mutually exclusive paths are needed    Order paths from most restrictive to least restrictive    Write conditional statement with Boolean expression    Follow with true path including action in indentation    Repeat 3 and 4 until all paths are included    (Optional) Follow with else path including action in indentation        Problem: Write the Python conditionals to solve the following specifications:   If integer variable current is odd, change its value so that it is now 4 times current plus 1; otherwise change its value so that it is now half of current (rounded down when current is odd).     1. Define how many mutually exclusive paths are needed  In this case, the problem says to do one action if variable is odd, and a different action otherwise.  An integer can only be odd or even, so there are two mutually exclusive paths.    2. Order paths from most restrictive to least restrictive  Since there are only two branches, the order does not matter.    3. Write conditional statement with Boolean expression  To determine if the current variable is odd, we can check that the remainder (modulo) after dividing by 2 is one:   if current % 2 == 1:     4. Follow with true path including action in indentation  In the True branch, we multiply the variable by 4 and add 1.   if current % 2 == 1: current = current * 4 + 1     5. Repeat 3 and 4 until all paths are included  N\/A since there is only the true and else path, which is below.    6. (Optional) Follow with else path including action in indentation  In the else branch, we change the value to be half of its current value. We must make sure we keep the result as an integer, so we use integer division.   if current % 2 == 1: current = current * 4 + 1 else: current \/\/= 2     if current % 2 == 1: current = current * 4 + 1 else: current \/\/= 2    "
+},
+{
+  "id": "conditional-wc-p1",
+  "level": "1",
+  "url": "conditional-wc-p1.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "WriteConditional-Exercise",
+  "body": " WriteConditional-Exercise   Subgoals for writing conditionals:     Define how many mutually exclusive paths are needed    Order paths from most restrictive to least restrictive    Write conditional statement with Boolean expression    Follow with true path including action in indentation    Repeat 3 and 4 until all paths are included    (Optional) Follow with else path including action in indentation        WrCond-Parsons-Problem    Put the code in the right order to create a program that will ask the user for 3 numbers (1 to 10 inclusive) for variables a, b, and c, and print the value of each variable. Then compare a to b, a to c, and b to c, and print statements for each comparison, reporting for each pair of variables if they are equal, greater than, or less than.    a = int(input(\"Please enter an integer between 1 and 10: \"))  b = int(input(\"Please enter an integer between 1 and 10: \"))  c = int(input(\"Please enter an integer between 1 and 10: \"))   print(a)  print(b)  print(c)  if a == b:   print(\"a and b are equal\")   elif a > b:  print(\"a is greater than b\")  else:  print(\"a is less than b\")  if a == c:   print(\"a and c are equal\")   elif a > c:  print(\"a is greater than c\")  else:  print(\"a is less than c\")   if b == c:   print(\"b and c are equal\")   elif b > c:   print(\"b is greater than c\")   else:   print(\"b is less than c\")      "
+},
+{
+  "id": "parsons-WrCond-WE2-P1-9",
+  "level": "2",
+  "url": "conditional-wc-p1.html#parsons-WrCond-WE2-P1-9",
+  "type": "Exercise",
+  "number": "2.2.1.1",
+  "title": "",
+  "body": " Put the code in the right order to create a program that will ask the user for 3 numbers (1 to 10 inclusive) for variables a, b, and c, and print the value of each variable. Then compare a to b, a to c, and b to c, and print statements for each comparison, reporting for each pair of variables if they are equal, greater than, or less than.    a = int(input(\"Please enter an integer between 1 and 10: \"))  b = int(input(\"Please enter an integer between 1 and 10: \"))  c = int(input(\"Please enter an integer between 1 and 10: \"))   print(a)  print(b)  print(c)  if a == b:   print(\"a and b are equal\")   elif a > b:  print(\"a is greater than b\")  else:  print(\"a is less than b\")  if a == c:   print(\"a and c are equal\")   elif a > c:  print(\"a is greater than c\")  else:  print(\"a is less than c\")   if b == c:   print(\"b and c are equal\")   elif b > c:   print(\"b is greater than c\")   else:   print(\"b is less than c\")   "
+},
+{
+  "id": "write_loops",
+  "level": "1",
+  "url": "write_loops.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Write Loops",
+  "body": " Write Loops   Subgoals for Writing a Loop    Determine purpose of loop    Pick a loop structure (while, for, do_while)      Define and initialize variables    Determine termination condition    Invert termination condition to continuation condition      Write the loop body    Update Loop Control Variable to reach termination         Problem: Write a loop that will prompt the user to continue to enter numbers until a sentinel value ( -1 ) is entered, and then find the maximum value entered.    1. Determine purpose of loop to pick loop structure (for, while)  Generally, while loops are a good choice for sentinel loops, since you cannot know up front how many times the code will need to iterate.    2. Define variables before loop that have dependencies within the loop  The user must input at least one number in order for us to find a maximum. The variable user_input will also be used as the loop control variable, so we must define it before the loop begins.   user_input = int(input(\"Enter a value, enter -1 to end.\"))   The maximum value must be updated during the loop based on its previous value, and will be printed after the loop, so that variable will need to be initialized prior to the loop.   maximum = user_input     3. Based on loop structure  The terminiation condition for this loop is when the user's inputted value is equal to the sentinel. We need to invert that condition to identify the continuation condition instead. That continuation condition must involve the loop control variable, which in this case will be user_input .   while user_input != -1:     4. Write loop body  First, we must check whether the latest maximum is greater than the user_input ; if so, we must update the maximum . This check is unnecessary during the first iteration of the loop, but is critical on every subsequent iteration.   while user_input != -1: if user_input > maximum: maximum = user_input   We then need to update the user_input loop control variable so that the loop has a chance to end at some point based on the user's value.   while user_input != -1: if user_input > maximum: maximum = user_input user_input = int(input(\"Enter a value, enter -1 to end.\"))     user_input = int(input(\"Enter a value, enter -1 to end.\")) maximum = user_input while user_input != -1: if user_input > maximum: maximum = user_input user_input = int(input(\"Enter a value, enter -1 to end.\")) print(maximum)     "
+},
+{
+  "id": "loop-wrl-p1",
+  "level": "1",
+  "url": "loop-wrl-p1.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "WriteLoops-Exercise",
+  "body": " WriteLoops-Exercise   Subgoals for Writing a Loop    Determine purpose of loop    Pick a loop structure (while, for, do_while)      Define and initialize variables    Determine termination condition    Invert termination condition to continuation condition      Write the loop body    Update Loop Control Variable to reach termination          Fill in the blanks in the following code to create a program that will print the prime numbers between 1 and 100 (inclusive).    for i in range( 2, 101):    is_prime = False   for j in range( 2, i):   if i % j == 0:   is_prime = True   if is_prime:   print(i, end=\" \")     "
+},
+{
+  "id": "parsons-Loop-Q1",
+  "level": "2",
+  "url": "loop-wrl-p1.html#parsons-Loop-Q1",
+  "type": "Exercise",
+  "number": "3.2.1",
+  "title": "",
+  "body": " Fill in the blanks in the following code to create a program that will print the prime numbers between 1 and 100 (inclusive).    for i in range( 2, 101):    is_prime = False   for j in range( 2, i):   if i % j == 0:   is_prime = True   if is_prime:   print(i, end=\" \")   "
 }
 ]
 
